7bad0acbaedf602fa4c76cd27acb99d6589323ee
DEVELOPER M7S <aryanvindraldh@gmail.com>
2026-06-30 00:17:25 +0530

    Change TOKEN value in main.py
    Updated the TOKEN environment variable for security.

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ def keep_alive():
 # --- End Flask Keep Alive ---
 
 # --- Configuration FROM .env FILE ---
-TOKEN = os.getenv('TOKEN', '8285024887:AAEJ6Jc5yEGVo_Z1OPfzsjbCMwqYAngdTUI')
+TOKEN = os.getenv('TOKEN', '8285024887:AAGJj_1WlDgzmOswlatqJI2XmAIkAEgyYzU')
 OWNER_ID = int(os.getenv('OWNER_ID', 6893661111 ))
 ADMIN_ID = int(os.getenv('ADMIN_ID', 6893661111))
 YOUR_USERNAME = os.getenv('YOUR_USERNAME', '@M7S_7M')
